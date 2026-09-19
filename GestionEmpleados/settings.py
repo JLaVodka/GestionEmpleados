11ad34da76ed22fb5f015d9d811ac25775086edb
Andres Delgadillo B. <129786979+JLaVodka@users.noreply.github.com>
2026-09-18 20:26:47 -0500

    Update ALLOWED_HOSTS for production settings

--- GestionEmpleados/settings.py
+++ GestionEmpleados/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gestionempleados-20ml.onrender.com', 'localhost']
 
 
 # Application definition
