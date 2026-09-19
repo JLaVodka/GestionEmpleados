c599940a85fa54c2d96b346e7889a921d5104580
Andres Delgadillo B. <129786979+JLaVodka@users.noreply.github.com>
2026-09-18 22:16:35 -0500

    Configure database settings and disable debug mode
    Updated settings to use dj_database_url for database configuration and set DEBUG to False for production.

--- GestionEmpleados/settings.py
+++ GestionEmpleados/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gestionempleados-20ml.onrender.com', 'localhost']
 
@@ -79,9 +81,9 @@ WSGI_APPLICATION = 'GestionEmpleados.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
     }
 }
 
