87100ac92e1db632d12b605804f97d9e21dbfe60
Andres Delgadillo B. <129786979+JLaVodka@users.noreply.github.com>
2026-09-26 09:11:09 -0500

    Add edit and delete paths for tasks

--- tareas/urls.py
+++ tareas/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<int:tarea_id>/', views.detalle_tarea, name='detalle_tarea'),
     path('asignada/', views.tarea_asignada, name='tarea_asignada'),
     path('consultar-ia/', views.consultar_ia, name='consultar_ia'),
+    path('<int:tarea_id>/editar/', views.editar_tarea, name='editar_tarea'),
+    path('<int:tarea_id>/eliminar/', views.eliminar_tarea, name='eliminar_tarea'),
     ]
