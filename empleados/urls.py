28c8598285d17453451cb7025b7820db5e4e9ce2
Andres Delgadillo B. <129786979+JLaVodka@users.noreply.github.com>
2026-09-26 09:24:26 -0500

    Add new routes for employee management

--- empleados/urls.py
+++ empleados/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.lista_empleados, name='lista_empleados'),
     path('<int:empleado_id>/', views.detalle_empleado, name='detalle_empleado'),
+    path('nuevo/', views.crear_empleado, name='crear_empleado'),
+    path('<int:empleado_id>/editar/', views.editar_empleado, name='editar_empleado'),
+    path('<int:empleado_id>/eliminar/', views.eliminar_empleado, name='eliminar_empleado'),
     ]
